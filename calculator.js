857f8d76458b8c6f10f3e77cfb768041e83022c9
oddtO <121516833+oddtO@users.noreply.github.com>
2023-03-06 14:43:11 +0200

    implement calculations

--- calculator.js
+++ calculator.js
@@ -1,20 +1,42 @@
+import * as calculatorMath from "./math.js"
+
+
+
 export function createCalculator(calculatorElement)
 {
 	let proto = {
 		render()
 		{
-			calculatorElement.querySelector(".num").textContent = this.displayedNum;		
+			calculatorElement.querySelector(".stored-num").textContent = this.storedNum == 0 ? "" : this.storedNum + " " + this.storedOperation;
+
+			let numElem = calculatorElement.querySelector(".num");
+				
+			
+			let trimmedValue = this.displayedNum.replace(/^0+/, "");
+			if(trimmedValue.length == 0)
+				trimmedValue = "0";
+
+			numElem.textContent = trimmedValue;
 		}, 
 
 
+		add: calculatorMath.add,
+		subtract: calculatorMath.subtract,
+		multiply: calculatorMath.multiply,
+		divide: calculatorMath.divide,
+		
+
+		
 
 	}
 
 
-	let calculator = Object.assign(Object.create(proto), {displayedNum: ""});
+	let calculator = Object.assign(Object.create(proto), {displayedNum: "0", storedNum: 0, currentOperation: ""});
+
 
 	calculatorElement.querySelector(".insert-buttons").addEventListener("click", handleInsert.bind(calculator));
 
+	calculatorElement.querySelector(".operation-buttons").addEventListener("click", handleOperation.bind(calculator));
 	return calculator;
 	
 }
@@ -28,11 +50,64 @@ function handleInsert(event)
 	const insertDigit = (digit) => this.displayedNum += digit;
 	const removeLastDigit = () => this.displayedNum = this.displayedNum.slice(0, -1);
 
-	if (event.target.textContent == "<")
+	if (event.target.dataset.value == "<")
 		removeLastDigit();
 	else
-		insertDigit(event.target.textContent);		
+		insertDigit(event.target.dataset.value);		
+
 	this.render();
 	
 
 }
+
+
+
+function handleOperation(event)
+{
+	if (event.target == event.currentTarget)
+		return;
+
+
+	let currentOperation = event.target.dataset.operation;
+
+	
+
+	const operate = (operation) => {
+
+		
+		if(this.displayedNum == "0")
+		{
+			return;
+		}
+		if(!this.storedNum)
+		{
+			this.storedNum = parseFloat(this.displayedNum);
+		}
+		else
+		{
+			this.storedNum = this[this.storedOperation](this.storedNum, parseFloat(this.displayedNum));
+			this.storedOperation = event.target.dataset.operation;
+
+		}
+		this.displayedNum = "0";
+	}
+
+		
+	if(currentOperation == "equals")
+	{
+		operate(this.storedOperation);
+	}
+	else
+	{
+		operate(currentOperation);
+
+		this.storedOperation = currentOperation;
+	}
+
+	this.render();
+	
+
+	
+
+}
+
