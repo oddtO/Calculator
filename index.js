fcb52050bfbd49637f5637cb32ab27f7f9dfbf14
oddtO <121516833+oddtO@users.noreply.github.com>
2023-03-07 10:49:50 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import {createCalculator} from './calculator.js'
-
-function operate(operator, num1, num2)
-{
-	return operator(num1, num2);
-}
-
-let calculator = createCalculator(document.querySelector(".calculator"));
